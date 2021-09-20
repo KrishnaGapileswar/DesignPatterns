eacd093a9af391b2869ffd219a09120b767bf79b
Krishna Gapileswar <kabikrish2797@gmail.com>
2021-09-20 17:50:02 +0530

    Create MovementBehavior.h

---
+++ MovementBehavior.h
@@ -0,0 +1,14 @@
+#ifndef MOVEMENT_BEHAVIOR
+#define MOVEMENT_BEHAVIOR
+
+class MovementBehavior
+{
+public:
+	virtual void move() = 0;
+	virtual ~MovementBehavior()
+	{
+
+	}
+};
+
+#endif //MOVEMENT_BEHAVIOR
